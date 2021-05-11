a8c8b0ded8854a63bf9d9c4d5c6eab97aa54bb5e
DanielDelev <1821027@stud.hs-mannheim.de>
2021-05-11 20:50:20 +0200

    Visualisierung mit PyGame

--- pygameTest.py
+++ pygameTest.py
@@ -1,6 +1,6 @@
 import pygame, sys
 from pygame.locals import *
-import Aufgabe_2 as gnn
+import Aufgabe2_1105 as gnn
 
 CANVAS_WIDTH = 300
 CANVAS_HEIGHT = 300
@@ -11,10 +11,11 @@ def main():
 
     #init canvas
     pygame.init()
-    canvas = pygame.display.set_mode((CANVAS_WIDTH,CANVAS_HEIGHT))
+    canvas = pygame.display.set_mode((CANVAS_WIDTH*2,CANVAS_HEIGHT*2))
 
     epoch_counter = 0
-    gnn.init_weights()
+    gnn.init_network()
+    testdata = gnn.generate_testdata(1000)
 
     while True:
 
@@ -24,26 +25,24 @@ def main():
                 pygame.quit()
                 sys.exit()
 
-        #update x and y values
-
-        if epoch_counter <= gnn.EPOCHS:
+        if epoch_counter <= 1000:
             epoch_counter += 1
             
             print(f"Epoch: {epoch_counter}")
 
             #training
-            gnn.train()
+            gnn.train_one_epoch_for_drawing(testdata)
             
             #drawing
-            x = 0
+            x = -1
             while x <= 1:
-                y = 0
+                y = -1
                 while y <= 1:
 
                     #calculate color value for x and y with neural network
-                    output = gnn.get_neural_output_final(x, y)
+                    output = gnn.get_network_output([x,y])[1][0]
                     color = (output*255, output*255, output*255)
-                    pos_x = x*CANVAS_WIDTH
+                    pos_x = x*CANVAS_WIDTH + CANVAS_WIDTH
                     pos_y = CANVAS_HEIGHT - RESOLUTION - y*CANVAS_HEIGHT #substract because we want (0,0) in bottom left corner
 
                     #draw pixels
